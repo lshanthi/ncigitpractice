52c7512dc9dc7cafef4263f8338dc5a39a2f7d2f
lshanthi <prashanthirajulagidi@gmail.com>
2022-01-30 18:25:52 +0000

    new file

---
+++ practice.sh
@@ -0,0 +1,7 @@
+roles and responsibility of the github
+if the new projects comes
+1. create a respository
+2.we have create the team and add the resources to the team and provide the access to the team
+3. so that who ever in the team they have access to the repository.
+
+
